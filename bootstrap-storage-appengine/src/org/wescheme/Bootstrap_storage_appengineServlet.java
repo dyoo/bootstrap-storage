1f3b194b60e0f3048404b1f4d652914defb10f0b
Danny Yoo <dyoo@cs.wpi.edu>
2012-06-11 15:26:55 -0400

    adding quick and dirty login

--- bootstrap-storage-appengine/src/org/wescheme/Bootstrap_storage_appengineServlet.java
+++ bootstrap-storage-appengine/src/org/wescheme/Bootstrap_storage_appengineServlet.java
@@ -3,6 +3,20 @@ package org.wescheme;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Logger;
+
+
 @SuppressWarnings("serial")
 public class Bootstrap_storage_appengineServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
